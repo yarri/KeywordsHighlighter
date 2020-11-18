905eb7eea499e38e0a0658f41aac2c60f0209ed2
Jaromir Tomek <jaromir.tomek@gmail.com>
2020-11-18 16:58:11 +0100

    Coding the library!

--- src/keywords_highlighter.php
+++ src/keywords_highlighter.php
@@ -7,6 +7,7 @@ class KeywordsHighlighter {
 		"d" => "ď",
 		"e" => "éèêë",
 		"l" => "ľ",
+		"n" => "ň",
 		"o" => "óòôõö",
 		"r" => "ř",
 		"s" => "š",
@@ -25,6 +26,10 @@ class KeywordsHighlighter {
 		];
 
 		$this->default_options = $options;
+
+		foreach($this->char_map as $letter => $alternatives){
+			$this->char_map[mb_strtoupper($letter)] = mb_strtoupper($alternatives);
+		}
 	}
 
 	function highlight($text,$keywords){
@@ -34,6 +39,23 @@ class KeywordsHighlighter {
 		$closing_tag = $options["closing_tag"];
 		$words = [];
 		$word = $keywords;
+		$chars = [];
+		foreach(preg_split('//u',$word) as $ch){
+			if(strlen($ch)==0){ continue; }
+			if(isset($this->char_map[$ch])){
+				$chars[] = "[$ch".$this->char_map[$ch]."]";
+				continue;
+			}
+			foreach($this->char_map as $letter => $alternatives){
+				if(strpos($alternatives,$ch)!==false){
+					$chars[] = "[$ch$letter]";
+					continue(2);
+				}
+			}
+			$chars[] = $ch;
+		}
+		$word = join($chars);
+		
 		$out = preg_replace("/($word)/iu","$opening_tag\\1$closing_tag",$text);
 		return $out;
 	}
